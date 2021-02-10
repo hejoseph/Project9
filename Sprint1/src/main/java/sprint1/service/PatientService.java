fea0e09678833fd1939794391be38e4ded9cec9f
HE Joseph <joseph.he@axa.fr>
2021-02-10 10:41:16 +0100

    CRUD patient + UI + docker compose

---
+++ Sprint1/src/main/java/sprint1/service/PatientService.java
@@ -0,0 +1,35 @@
+package sprint1.service;
+
+import sprint1.model.Patient;
+import org.springframework.stereotype.Service;
+import sprint1.repository.PatientRepository;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public List<Patient> findAll() {
+        return patientRepository.findAll();
+    }
+
+    public Patient deletePatient(String id) {
+        Patient patient = patientRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid bidlist Id:" + id));
+        patientRepository.delete(patient);
+        return patient;
+    }
+
+    public Patient findById(String id) {
+        return patientRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid bid Id:" + id));
+    }
+}
